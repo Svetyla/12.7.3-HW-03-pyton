369fee788648b22a347ac26a6cdb567f1ba7a7c4
Svetyla <sveta200892@gmail.com>
2023-02-10 16:45:17 +0300

    add file

---
+++ script.py
@@ -0,0 +1,10 @@
+money = int(input('введите сумму'))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+deposit.append(int(money*per_cent['ТКБ']/100))
+deposit.append(int(money*per_cent['СКБ']/100))
+deposit.append(int(money*per_cent['ВТБ']/100))
+deposit.append(int(money*per_cent['СБЕР']/100))
+print(deposit)
+max_deposit = max(deposit)
+print("Максимальная сумма, которую вы можете заработать — " + str(max_deposit))
